3e1126a711324c9b43137ae1fb59502852e93708
José Manuel Díez <j.diezlopez@tu-berlin.de>
2024-02-27 13:48:55 +0100

    Add example_rust_node

---
+++ src/example_rust_node/src/main.rs
@@ -0,0 +1,23 @@
+use example_messages::msg::HelloWorld as HelloMsg;
+
+fn main() -> Result<(), rclrs::RclrsError> {
+    let context = rclrs::Context::new(std::env::args())?;
+    let node = rclrs::Node::new(&context, "example_rust_node")?;
+    let publisher = node.create_publisher::<HelloMsg>("example_topic", rclrs::QOS_PROFILE_DEFAULT)?;
+
+    let mut cnt: i64 = 0;
+
+    while context.ok() {
+        let mut msg = HelloMsg::default();
+        msg.num = cnt;
+        msg.frac = (cnt as f64)/10.0;
+        cnt += 1;
+
+        println!("Publishing {:?}", &msg);
+        publisher.publish(msg)?;
+        
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+    }
+
+    Ok(())
+}
